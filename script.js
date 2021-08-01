c55e418c0572a0149aed0e0c05131d50c3bea867
MillieWilkins <83929585+MillieWilkins@users.noreply.github.com>
2021-08-01 21:11:08 +1000

    Called API for daily weather forecast and edited the response to be more user friendly!

--- script.js
+++ script.js
@@ -25,33 +25,49 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(weekday) {
+  let weekDay = new Date(weekday * 1000);
+  let day = weekDay.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 let dateElement = document.querySelector("#date");
 let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
 
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = ` <div class="row">`;
-  let days = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      ` <div class="col-2">
-        <div class="forecast-date">${day}</div>
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        ` <div class="col-2">
+        <div class="forecast-date">${formatDay(forecastDay.dt)}</div>
         <img 
-          src="http://openweathermap.org/img/wn/50d@2x.png"
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
           alt=""
-          width="50"
+          width="40"
         />
         <div class="forecast-temp">
-          <span class="forecast-temp-max"> 18° </span>
-          <span class="forecast-temp-min"> 12° </span>
+          <span class="forecast-temp-max">${Math.round(
+            forecastDay.temp.max
+          )}º</span>
+          <span class="forecast-temp-min">${Math.round(
+            forecastDay.temp.min
+          )}º</span>
         </div>
       </div>
 
 `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
